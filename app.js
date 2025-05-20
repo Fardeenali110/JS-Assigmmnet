647e624f196dd855e0d72307eaf19400d9dde630
Fardeen Ali <133501548+Fardeenali110@users.noreply.github.com>
2025-05-20 12:12:57 +0500

    Add files via upload
    Assigmnet Js

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("Hello And Welcome to the Calculator!");
+alert("Hello And Welcome to the Calculator!");
+
+let num1=+prompt("Enter first number To Perform Addition");
+let num2=+prompt("Enter second number To Perform Addition");
+let result=num1+num2;
+
+console.log("Addition is done "+result);
+
+let Fnum=+prompt("Enter first number To Perform Subtraction");
+let Snum=+prompt("Enter second number To Perform Subtraction");
+let result1=(Fnum-Snum);
+console.log("Subtraction is done "+result1);
+
+
+let Num=+prompt("Enter first number To Perform Multiplication");
+let num=+prompt("Enter second number To Perform Multiplication");
+let result2=(Num*num);
+console.log("Multiplication is done "+result2);
+
+let Dnum1=+prompt("Enter first number To Perform Division");
+let Dnum2=+prompt("Enter second number To Perform Division");
+let result3=(Dnum1/Dnum2);
+console.log("Addition is done "+result);
+document.write("The Addition Results Are "+result+"<br>");
+document.write("The Subtrsction  Results Are "+result1+"<br>");
+document.write("The Multiplication Results Are done "+result2+"<br>");
+document.write("The Division is Results Are "+result3+"<br>");
+
